ebcacf13919ca0b976dfb2ade387ff5519f0866d
Nicholas Hannah <nicjhannah@gmail.com>
2015-06-05 19:03:32 -0700

    Further improvements to valgrind tests. #148

--- tools/tests/test_valgrind.py
+++ tools/tests/test_valgrind.py
@@ -37,9 +37,10 @@ class TestValgrind:
             output = sp.check_output(shlex.split(cmd), stderr=sp.STDOUT)
         except sp.CalledProcessError as e:
             ret = e.returncode
-            print(e.output, file=sys.stderr)
         finally:
+            with open('valgrind.out', 'r+') as f:
+                print(e.output, file=f)
             os.chdir(saved_path)
         assert(ret == 0)
 
-        # Check valgrind output
+        # Do checks on valgrind output.
